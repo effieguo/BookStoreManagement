c8056933e5d441677e1521c7051251bc29997b19
effieguo <effie0711@gmail.com>
2017-08-04 00:49:08 -0700

    Deleting some comments.

--- BookStoreManagement/Main.cpp
+++ BookStoreManagement/Main.cpp
@@ -1,5 +1,4 @@
 /*
-* Project: Implemente the cstring functions.
 * Description: Serendipity Booksellers is a small bookstore located in a shopping mall. They have a cashier station equipped with a personal computer. 
              The manager wants you to develop a point-of-sale (POS) software package that will make the computer function as a cash register and keep an inventory file.
 			 The inventory file will be a database of all the books in the bookstore. In general, the software is to perform the following tasks: 
